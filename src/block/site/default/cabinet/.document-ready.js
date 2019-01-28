4c54457634cd6eaa3629ef4cbf5f4b95276de34c
caezar47 <iss47@ya.ru>
2019-01-28 11:21:56 +0300

    add basket page

--- src/block/site/default/cabinet/.document-ready.js
+++ src/block/site/default/cabinet/.document-ready.js
@@ -2,3 +2,4 @@ var url = window.location.pathname;
 //var url = window.location.href;
 $('.cabinet__navbar-nav a[href="'+url+'"]').parent().addClass('is--active');
 $('.cabinet__navbar-block').closest('body').addClass('is--aside-cabinet');
+$('.cabinet__basket-total-bar').closest('body').addClass('is--basket-bar');
