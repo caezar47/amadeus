496adceb3937e7eefa484176556a5d9b52835393
caezar47 <iss47@ya.ru>
2019-01-31 11:27:06 +0300

    add index page

--- src/block/site/default/slick/.document-ready.js
+++ src/block/site/default/slick/.document-ready.js
@@ -2,6 +2,7 @@
 $(function() { 
 	var header = $('[data-slider-slick="slick-header"]');	
 	var gallery = $('[data-slider-slick="slick-gallery"]');	
+	var index_catalog = $('[data-slider-slick="slick-index-catalog"]');	
 	var catalog = $('[data-slider-slick="slick-catalog"]');	
 	var catalog_nav = $('[data-slider-slick="slick-catalog-nav"]');	
 	var CMS__TPL_PATH = '/wp-content/themes/azbn7theme';  
@@ -12,20 +13,75 @@ $(function() {
 	var nextArrow = '<button type="button" class="slick-btn  is--next"><span class="sr-only">Следующий слайд</span><svg class="icon-svg icon-icon-next" role="img"><use xlink:href="'+ CMS__TPL_PATH +'/img/svg/sprite.svg#icon-next"></use></svg></button>';	
 	
 	$('.slick-cloned .content-block__preview').removeAttr('data-fancybox');
-	
-	/*header.slick({
+
+	header.slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		infinite: true,
-		arrows: true,
-		dots: false,
-		//infinite: true, 
+		arrows: false,
+		dots: true,
 		autoplay: true,
   		autoplaySpeed: 4000,
 		prevArrow: prevArrow,
 		nextArrow: nextArrow,
 		fade: true
-	});*/
+	});
+	index_catalog.slick({
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		arrows: true,
+		dots: false,
+		infinite: true,
+		prevArrow: prevArrow,
+		nextArrow: nextArrow,
+		responsive: [
+		    {
+				breakpoint: 1025,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				}
+		    },
+		    {
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+		    },
+		    {
+				breakpoint: 565,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+		    }
+		]
+	});
+	var tabs = $(".tabs__link");
+	tabs.on('click', function(){
+	    var elem = $(this);
+	    var filter = elem.data('filter');
+	    index_catalog.slick('slickUnfilter');
+	    if(filter == 'new'){
+			index_catalog.slick('slickFilter','.is--new');
+	    	tabs.parent().removeClass('active');
+			elem.parent().addClass('active');
+	    }
+	    else if(filter == 'hit'){
+	     	index_catalog.slick('slickFilter','.is--hit');
+	    	tabs.parent().removeClass('active');
+			elem.parent().addClass('active');
+	    }
+	    else if(filter == 'discounts'){
+	     	index_catalog.slick('slickFilter','.is--discounts');
+	    	tabs.parent().removeClass('active');
+			elem.parent().addClass('active');
+	    }   
+ 	});
+	index_catalog.slick('slickFilter','.is--new');
+
+
 	gallery.slick({
 		slidesToShow: 4,
 		slidesToScroll: 4,
