ba7405725680215fa75f6fe94930d90e3d1b79a9
caezar47 <iss47@ya.ru>
2019-02-04 07:51:54 +0300

    ready frontend

--- src/block/site/default/cabinet/.document-ready.js
+++ src/block/site/default/cabinet/.document-ready.js
@@ -11,10 +11,13 @@ $(".cabinet__delivery-card").on('click', function(e) {
 $(".cabinet__delivery-transport").on('click', function(e) {
 	$(this).toggleClass('is--active');
 });	
+if(screenJS.isXS()) {
+	$('.cabinet__order-body').removeAttr('id');
+}	
 $('.cabinet__filter-nav-item.dropdown').on('click', '.cabinet__filter-nav-dropdown', function(e) {
     e.preventDefault();
     e.stopPropagation(); 
 });
 $('.cabinet__filter-btn-block').on('click', function(e) {
 	$(this).closest('.cabinet__filter-block').toggleClass('open');
-});
+});	
